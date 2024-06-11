5e8cada66432a5ac401328ac631f21b4b70ed074
sammy002621 <kalindasammy4@gmail.com>
2024-06-11 16:06:42 +0200

    adde full add and delete functionality

--- src/components/Results.tsx
+++ src/components/Results.tsx
@@ -1,19 +1,17 @@
-import { useState, useEffect,ChangeEvent } from 'react'
-import { Link, NavLink, useSearchParams} from 'react-router-dom';
-import "../App.css"
+
+import { useState, useEffect, ChangeEvent } from 'react';
+import { Link, NavLink, useSearchParams } from 'react-router-dom';
+import "../App.css";
 // import { Link } from 'react-router-dom';
 
 type User = {
-  id:number,
+  id: number,
   username: string,
-
 };
 
 function Results() {
-  
-
   const user1: User = {
-    id:1,
+    id: 1,
     username: "employee_1",
   };
 
@@ -21,87 +19,67 @@ function Results() {
 
   const [users, setUsers] = useState<User[]>(users_1);
   const [originalUsers, setOriginalUsers] = useState<User[]>(users_1);
-  const [searchParams] = useSearchParams();
-  const [selected,setSelected]=useState<string[]>([]);
-  const [rerender, setRerender] = useState(false);
-  const [removedIndex,setRemovedIndex]=useState<number>(0);
-
-  
-
- 
+  const [selected, setSelected] = useState<string[]>([]);
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then((data) => {
-        const userData:User[] = data.map((user:any)=>({
-          id:user.id,
-          username:user.username
+        const userData: User[] = data.map((user: any) => ({
+          id: user.id,
+          username: user.username
         }));
         setUsers(userData);
         setOriginalUsers(userData);
-        console.log("users are :",users);
+        console.log("users are :", users);
       });
-  }, []); 
-  
-  useEffect(()=>{
-  console.log("new users are:",selected);
+  }, []);
 
-  const selectIndex=selected.length-1;
-  
+  useEffect(() => {
+    console.log("new users are:", selected);
 
-  selected.map((select,index)=>{
-    if(index === selectIndex){
-  const newP=document.createElement('p');
-  newP.innerHTML=select;
-  newP.classList.add("class_select");
-  newP.classList.add("roboto-regular");
-  newP.setAttribute("id",`${index}`);
-  document.body.appendChild(newP);
-    }
-  })
+    selected.forEach((select)=>{
+      if(!document.getElementById(`user-${select}`)){
+        const newP=document.createElement('p');
+        newP.innerHTML=select;
+        newP.classList.add("class-select","roboto-regular");
+        newP.setAttribute("id",`user-${select}`)
+        document.body.appendChild(newP)
+      }
 
+      const selectedAllIds=selected.map(user=>`user-${user}`);
+      console.log(selectedAllIds);
+      document.querySelectorAll('p.class-select').forEach((element)=>{
+        if(!selectedAllIds.includes(element.id)){
+    element.remove();
+        }
+      })
 
-  // selected.map((select,index)=>{
-
-  // //   if(index === select.length-1){
-  // //     console.log(" the numbers are equal");
-  // //     const newP=document.createElement('p');
-  // //     newP.innerHTML=select;
-  // //     document.body.appendChild(newP)
-  // //   }else{
-
-  // //   }
-  // //   console.log("after the condition")
-  // // })
-
-  
-      
-    
-  },[selected])
-
-
-
-  
-  
-  
-  // Changed dependency to an empty array to avoid infinite loop
-
-
-const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
-  const searchQuery = e.target.value.toLocaleLowerCase();
-  console.log("the search query is :",searchQuery);
-  if(searchQuery){
-    const filteredUsers = originalUsers.filter((user)=>{
-     return user.username.toLocaleLowerCase().includes(searchQuery);
     })
-    console.log("filtered users are:",filteredUsers)
-    setUsers(filteredUsers);
-  }else{
-    setUsers(originalUsers);
-  }
- 
-  }
+
+    // Remove unselected usernames
+    const allSelectedIds = selected.map(user => `user-${user}`);
+    document.querySelectorAll('p.class_select').forEach(element => {
+      if (!allSelectedIds.includes(element.id)) {
+        element.remove();
+      }
+    });
+  }, [selected]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const searchQuery = e.target.value.toLocaleLowerCase();
+    console.log("the search query is :", searchQuery);
+    if (searchQuery) {
+      const filteredUsers = originalUsers.filter((user) => {
+        return user.username.toLocaleLowerCase().includes(searchQuery);
+      });
+      console.log("filtered users are:", filteredUsers);
+      setUsers(filteredUsers);
+    } else {
+      setUsers(originalUsers);
+    }
+  };
+
   return (
     <>
       <div className='body_div'>
@@ -109,43 +87,26 @@ const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
           {/* container_search_bar */}
           <div className='search'>
             <input type="text" placeholder='Search' id='search' onChange={handleChange} />
-            {/* 
-            get the value in the input
-            compare that value with all the words in our array
-            return all the values that have atleast a character in the search word
-            setUsers(newArray)
-
-            
-            keep track of the index that has been removed and then update the state to reflect the changes
-            
-            
-            
-            */}
           </div>
           {/* container_body */}
           <div className='container_body'>
             {users.map((user) => (
-              <div  key={user.id} id='clicked' className={(selected.includes(user.username.toLocaleLowerCase())) ? `container_body_name` : `roboto-regular`} onClick={()=>{
-  if(!selected.includes(user.username.toLocaleLowerCase())){
-   setSelected((prev:string[])=>[...prev,user.username.toLocaleLowerCase()]);
-
-    
-    // let newP = document.createElement("p");
-    // newP.innerHTML = user.username;
-    // document.getElementById("search")?.appendChild(newP);
-  }else{
-   setSelected((prevSelected)=>{
-    return prevSelected.filter((select,index)=>{
-      setRemovedIndex(index);
-      return select!==user.username.toLocaleLowerCase()
-    });
-    })
-
-
-    
-  }
-  setRerender(prev => !prev);
-              }} >
+              <div
+                key={user.id}
+                id='clicked'
+                className={selected.includes(user.username.toLocaleLowerCase()) ? `container_body_name` : `roboto-regular`}
+                onClick={() => {
+                  if (!selected.includes(user.username.toLocaleLowerCase())) {
+                    setSelected((prev: string[]) => [...prev, user.username.toLocaleLowerCase()]);
+                  } else {
+                    setSelected((prevSelected) => {
+                      return prevSelected.filter((select) => {
+                        return select !== user.username.toLocaleLowerCase();
+                      });
+                    });
+                  }
+                }}
+              >
                 {user.username}
               </div>
             ))}
@@ -155,9 +116,7 @@ const handleChange = (e:ChangeEvent<HTMLInputElement>)=>{
         </div>
       </div>
     </>
-  )
-
-  }
-
+  );
+}
 
 export default Results;
